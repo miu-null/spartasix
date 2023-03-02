198570e5d1437575c5f9ba7c389c2b56a8c76028
vividnow <belucent0@gmail.com>
2023-03-02 23:04:54 +0900

    [추가] ejs 초기 셋팅

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule
+    );
+
+  
   app.useGlobalPipes(new ValidationPipe({transform: true}));
+  
+  //ejs 셋팅
+  app.useStaticAssets(join(__dirname, '..', 'src', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'src', 'views'));
+  app.setViewEngine("ejs");
+
   await app.listen(3000);
 }
 bootstrap();
