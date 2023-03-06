63c3cae8ce05dcd5be7fdad7c6e7873dfb3c1d66
kangharu <harucometrue7@gmail.com>
2023-03-06 20:05:27 +0900

    event create api(in proceeding...)

---
+++ src/event/event.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { EventRepository } from "./event.repository"; 
+
+@Injectable()
+export class EventService {
+
+    constructor(private userRepository: EventRepository) {}
+    
+    async createEvent(
+        userId: string,
+        title: string,
+        content: string,
+        date: Date,
+        viewCount: number,
+      ) {
+        await this.userRepository.createEvent(
+            userId,
+            title,
+            content,
+            date,
+        );
+      }
+}
