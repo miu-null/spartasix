e9a4c0a79185567e819ace812cc0f00258c159bc
kangharu <harucometrue7@gmail.com>
2023-03-07 19:51:07 +0900

    이벤트 게시판 이벤트 글 수정,삭제 event api 추가

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -1,7 +1,49 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Param ,Get, Put,Delete} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { EventService } from "./event.service";
+import { CreateEventDto } from "./dto/createevent.dto";
+import { UpdateEventDto } from "./dto/updateevent.dto";
+import { DeleteEventDto } from "./dto/deleteevent.dto";
 
 @Controller('event')
 export class EventController {
+        constructor(private eventService: EventService) {}
+    
+        @Post("/events")
+        async createUser(@Body() data: CreateEventDto) {
+          return this.eventService.createEvent(
+            data.userId,
+            data.title,
+            data.content,
+            data.date,
+            data.viewCount,
+          );
+        }
+        @Get('/articles/:id')
+        getArticleById(@Param('id') articleId: number) {
+          return this.eventService.getEventById(articleId);
+        }
+      
+      
+        @Put('/articles/:id')
+        updateArticle(
+          @Param('id') articleId: number,
+          @Body() data: UpdateEventDto,
+        ) {
+          return this.eventService.updateEvent(
+            articleId,
+            data.title,
+            data.content
+          );
+        }
+      
+        @Delete('/articles/:id')
+        deleteArticle(
+          @Param('id') articleId: number,
+          @Body() data: DeleteEventDto,
+        ) {
+          return this.eventService.deleteEvente(articleId, data.password);
+        }
 
     
 }
---
+++ src/event/event.repository.ts
@@ -0,0 +1,50 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { EventPosts } from "./entity/event.entity";
+
+@Injectable()
+export class EventRepository {
+  
+    constructor(
+    @InjectRepository(EventPosts)
+    private readonly eventRepository: Repository<EventPosts>,
+  ) {}
+
+  async createEvent(
+    userId: string,
+    title: string,
+    content: string,
+    date: string,
+  ) {
+    this.eventRepository.insert({
+        title,
+        content,
+        date,
+    });
+  }
+
+  async updateEvent(
+    id: string,
+    title: string,
+    content: string,
+    date: string,
+  ) {
+    this.eventRepository.update({
+      id,
+        title,
+        content,
+        date,
+    });
+  }
+
+  async softDeleteEvent(
+    id: string,
+  ) {
+    this.eventRepository.softDelete({
+      id,
+    });
+  }
+
+
+}
