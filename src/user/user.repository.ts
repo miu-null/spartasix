4cd31fa6fd9b8070e5f67c488244320ccb860a2f
ByunJungMin <cvy5375@naver.com>
2023-03-08 23:16:30 +0900

    [추가] 비밀번호 암호화

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -7,6 +7,7 @@ import { JwtService } from "@nestjs/jwt";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Users } from "../entities/users.entity";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class UserRepository {
@@ -39,7 +40,7 @@ export class UserRepository {
     }
   }
 
-  async login(email: string, password: string) {
+  async login(email: string) {
     const user = await this.userRepository.findOne({
       where: { email, deletedAt: null },
       select: ["userId", "email", "password"],
@@ -49,14 +50,10 @@ export class UserRepository {
       throw new NotFoundException("회원이 존재하지 않습니다.");
     }
 
-    if (user.password !== password) {
-      throw new UnauthorizedException("비밀번호가 올바르지 않습니다.");
-    }
-
     const payload = { id: user.userId };
     const accessToken = await this.jwtService.signAsync(payload);
 
-    return accessToken;
+    return {...user, accessToken};
   }
 
   async checkThisUser(userId: number) {
@@ -86,4 +83,6 @@ export class UserRepository {
 
     return nickname;
   }
+
+
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,6 +4,7 @@ import {
   UnauthorizedException,
 } from "@nestjs/common";
 import { UserRepository } from "./user.repository";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class UserService {
@@ -25,15 +26,29 @@ export class UserService {
         "비밀번호와 비밀번호 확인란이 일치하지 않습니다.",
       );
     }
+    const hashpassword = await this.transformPassword(password);
 
-    await this.userRepository.createUser(email, password, nickName, phone);
+    await this.userRepository.createUser(email, hashpassword, nickName, phone);
   }
 
   async login(email: string, password: string) {
-    return this.userRepository.login(email, password);
+    const user = await this.userRepository.login(email);
+    const validatePassword = await bcrypt.compare(password, user.password);
+
+    if (validatePassword === false) {
+      throw new UnauthorizedException("비밀번호가 올바르지 않습니다.");
+    }
+
+    return user.accessToken;
   }
 
   async checkNickname(nickName: string) {
     return this.userRepository.checkNickname(nickName);
+  }
+
+  async transformPassword(password: string) {
+    const hashpassword = await bcrypt.hash(password, 10);
+
+    return hashpassword;
   }
 }
