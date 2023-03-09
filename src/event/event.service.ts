4fbb098b4399ce781542bcc82abc7e5d6c8a7f17
miu-null <118172695+miu-null@users.noreply.github.com>
2023-03-09 09:52:47 +0900

    [수정] 코드 복구

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,25 +1,38 @@
 import { Injectable } from '@nestjs/common';
-import { EventRepository } from "./event.repository";
+import { EventRepository } from "./event.repository"; 
 
 @Injectable()
 export class EventService {
 
-  constructor(private EventRepository: EventRepository) { }
+    constructor(private EventRepository: EventRepository) {}
+    
+    async createEvent(
+        userId: string,
+        title: string,
+        content: string,
+        date: string,
+        viewCount: number,
+      ) {
+        await this.EventRepository.createEvent(
+            userId,
+            title,
+            content,
+            date,
+        );
+      }
 
-  async createEvent(
-    userId: string,
-    title: string,
-    content: string,
-    date: string,
-    viewCount: number,
-  ) {
-    await this.EventRepository.createEvent(
-      userId,
-      title,
-      content,
-      date,
-    );
-  }
+      async updateEvent(
+        id: number,
+        title: string,
+        content: string,
+
+      ) {
+        this.EventRepository.updateEvent(id, { title, content });
+      }
+
+      async deleteEvente(id: number) {
+        this.EventRepository.softDeleteEvent(id);
+      }
 
 
 }
