de46a6760a0b1278aa13bf678626e170a78d7654
ByunJungMin <cvy5375@naver.com>
2023-03-09 20:12:42 +0900

    [수정] user폴더 내용 auth폴더로 이동 및 user폴더 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,19 @@
 import {
-  MiddlewareConsumer,
   Module,
-  NestModule,
-  RequestMethod,
 } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { JwtModule } from "@nestjs/jwt";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { AuthMiddleware } from "./auth/auth.middleware";
-// import { ClubModule } from "./club/club.module";
 import { JwtConfigService } from "./config/jwt.config.service";
 import { typeOrmConfigService } from "./config/typeorm.config.service";
 import { SearcherModule } from "./searcher/searcher.module";
 import { ClubModule } from "./club/club.module";
-import { UserModule } from "./user/user.module";
 import { EventModule } from "./event/event.module";
 import { UserpageModule } from "./userpage/userpage.module";
-// import { AuthModule } from './auth/auth.module';
+import { MailModule } from './mail/mail.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -34,25 +29,15 @@ import { UserpageModule } from "./userpage/userpage.module";
       inject: [ConfigService],
     }),
 
-    // EventModule,
-    UserModule,
+    EventModule,
     SearcherModule, //김재광 검색기능 테스트
     ClubModule,
     UserpageModule,
-    // AuthModule,
+    MailModule,
+    AuthModule,
 
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(AuthMiddleware)
-      .forRoutes(
-        { path: "user/update", method: RequestMethod.PATCH },
-        { path: "userpage/info/:userId", method: RequestMethod.GET },
-        { path: "userpage/info/:userId", method: RequestMethod.PATCH },
-      );
-  }
-}
+export class AppModule {}
