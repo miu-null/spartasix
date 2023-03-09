aee2c784649a21c0c1d82d42d9fc3915797a0089
vividnow <belucent0@gmail.com>
2023-03-09 14:41:02 +0900

    [추가] 회원 검색기능(회원번호, 닉네임)

--- src/searcher/searcher.service.ts
+++ src/searcher/searcher.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateSearchDto } from './dto/create.search.dto';
-import { Searcher} from './entity/searcher.entity';
+import { Searcher} from '../entities/searcher.entity';
 import { SearcherRepository } from './searcher.repositoy';
-import { IsUUID } from 'class-validator';
-
+import { UserSearchRepository } from './searcher.repositoy';
 
 @Injectable()
 export class SearcherService {
+    searchArticle(createSearchDto: CreateSearchDto) {
+    throw new Error('Method not implemented.');
+  }
     constructor(private SearcherRepository: SearcherRepository) {}  
 
     findEventPosts(term : any) {
@@ -17,11 +19,8 @@ export class SearcherService {
         return results
     }
 
-    // findOne(id) : Promise<Searcher[]> {
-    //     return this.SearcherRepository.find(id);
-    // }
 
-    async createArticle(createSearchDto:CreateSearchDto) {
+    async Articlecreate(createSearchDto:CreateSearchDto) {
         const {title, content} = createSearchDto
         const article = {
             title,
@@ -29,32 +28,20 @@ export class SearcherService {
             createdAt : new Date(),
         }
         console.log('크리에이트, 서비스');
-        return await this.SearcherRepository.ArticleCreate(createSearchDto)}
-
-        
-    //     await this.SearcherRepository.save(articles);
-    //     return articles;
-    // }
-
-    // async remove(id: number) : Promise<void> {
-    //     await this.SearcherRepository.delete(id);
-    // }
+        return await this.SearcherRepository.ArticleCreate(createSearchDto)
+    }
 }
 
-// @Injectable()
-// export class SearcherService {
-//     constructor(
-//         @InjectRepository(Searcher)
-//         private SearchserRepository: Repository<Searcher>, //Searcher 라는 엔티티를 레파지토리로 사용할 수 있게 됨
-//     ) {}
 
-//     findAll() : Promise<Searcher[]> {
-//         return this.SearchserRepository.find();
-//     }
+@Injectable()
+export class UserSearchService {
+  constructor(private userSearchRepository: UserSearchRepository) {}
 
+findusers(term: any) {
+    console.log(term, '서비스')
+    const results = this.userSearchRepository.findusers(term);
+    console.log(term, '리포리토지 통과')
+    return results
+}
 
-
-//     async remove(id: number) : Promise<void> {
-//         await this.SearchserRepository.delete(id);
-//     }
-
+}
