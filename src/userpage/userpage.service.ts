2954a1d12d88c9f3cb4f5009b33cc6de497cc8d5
Hojin Kim <miu.null@gmail.com>
2023-03-10 02:30:35 +0900

    [수정] userpage 데이터베이스 쿼리 수정

--- src/userpage/userpage.service.ts
+++ src/userpage/userpage.service.ts
@@ -10,7 +10,7 @@ import { UserUpdateDto } from "./dto/userpage.update.dto";
 
 @Injectable()
 export class UserpageService {
-  constructor(private userPageRepository: UserPageRepository) { }
+  constructor(private userPageRepository: UserPageRepository) {}
 
   // // 작성한 글 조회
   // async getMyPosts(userId: number) {
@@ -18,15 +18,37 @@ export class UserpageService {
   // }
 
   // 유저정보 조회
-  async getUsersInfo(userId: number) {
-    return await this.userPageRepository.getUsersInfo(userId);
+  async getUserInfo(userId: number, user: any) {
+    console.log(user);
+    const data = await this.userPageRepository.getUserInfo(userId);
+    const password = data.password.length;
+
+    // 로그인한 user 정보가 있어야만 정보가 불러와지
+    // 아래 내용은 프론트에서 작업하기
+    if (userId !== user.id) {
+      return {
+        userIMG: data.userIMG,
+        nickName: data.nickName,
+        email: data.email,
+        snsURL: data.snsURL,
+      }; // 타 유저 조회
+    }
+    if (userId === user.id) {
+      return {
+        email: data.email,
+        password,
+        phone: data.phone,
+        nickName: data.nickName,
+        snsURL: data.snsURL,
+        userIMG: data.userIMG,
+      }; // 본인 조회
+    }
   }
 
   // 회원정보 수정
   async updateUser(userId: number, updateUserInfo: UserUpdateDto) {
     console.log(userId);
     // if (userId ===!)
-
     return await this.userPageRepository.updateUser(userId, updateUserInfo);
   }
 
@@ -35,24 +57,22 @@ export class UserpageService {
     return await this.userPageRepository.getMyClubs(userId);
   }
 
-
-  // 클럽 신청서 조회
+  // 클럽 신청서 조회 // 특정 유저만
   async getClubApps(userId: number) {
     return await this.userPageRepository.getClubApps(userId);
   }
 
+  // 특정 신청서 조회 // 특정 유저만
+  async getThisApp(userId: number, clubMemberId: number) {
+    return await this.userPageRepository.getThisApp(userId, clubMemberId);
+  }
 
-  // // 신청서 수락
-  // async getThisMember(userId: number, clubMemberId: number) {
-  //   return await this.userPageRepository.({
-  //     where: { userId },
-  //     select: [],
-  //   });
-  // }
+  // 신청서 수락 // 특정 유저만
+  async getThisMember(userId: number, clubMemberId: number) {
+    return await this.userPageRepository.getThisMember(userId, clubMemberId);
+  }
 
-  //   async getThisMember(userId: number, clubMemberId: number) {
-  //     return await this.clubRepository.find();
-  //   }
-  // async deleteApps(userId: number, clubMemberId: number) { }
-
+  async rejectApp(userId: number, clubMemberId: number) {
+    return await this.userPageRepository.rejectApp(userId, clubMemberId);
+  }
 }
