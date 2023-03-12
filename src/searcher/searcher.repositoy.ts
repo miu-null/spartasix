d065c30632c361e21c40e9b11e927826a681b9ac
vividnow <belucent0@gmail.com>
2023-03-13 00:14:58 +0900

    [추가] 통합 검색기능-테스트용

--- src/searcher/searcher.repositoy.ts
+++ src/searcher/searcher.repositoy.ts
@@ -1,9 +1,11 @@
 import { Injectable } from "@nestjs/common";
 import { getRepositoryToken, InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
-import { Searcher } from "../entities/searcher.entity";
 import { CreateSearchDto } from "./dto/create.search.dto";
+import { Searcher } from "../entities/searcher.entity";
 import { Users } from "../entities/users.entity";
+import { Clubs } from "../entities/clubs.entity";
+import { EventPosts } from "../entities/eventposts.entity";
 // import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
@@ -12,22 +14,58 @@ export class SearcherRepository {
     @InjectRepository(Searcher)
     private readonly searcherRepository: Repository<Searcher>,
     @InjectRepository(Users)
-    private readonly userSearchRepository: Repository<Users>
+    private readonly userSearchRepository: Repository<Users>,
+    @InjectRepository(Clubs)
+    private readonly clubsSearchRepository: Repository<Clubs>,
+    @InjectRepository(EventPosts)
+    private readonly eventSearchRepository: Repository<EventPosts>,
+
   ) {}
 
-  async findEventposts(data: any): Promise<Searcher[]> {
+  async findAllPosts(data: any): Promise<any> {  //테스트용 통합검색
     {
       console.log(`%${data.term}%`, data, "리포지토리 진입");
-      const results = await this.searcherRepository
+      const clubs = await this.clubsSearchRepository
         .createQueryBuilder('search')
         .where('search.title LIKE :s OR search.content LIKE :s', { s: `%${data.term}%` })
+        .getMany();
+      const events = await this.eventSearchRepository
+        .createQueryBuilder('search')
+        .where('search.title LIKE :s OR search.content LIKE :s', { s: `%${data.term}%` })
+        .getMany();
+      const results = {events, clubs}
+      console.log(results);
+      return results
+    }
+  }
+
+  async findEventPosts(data: any): Promise<EventPosts[]> { //event 게시글 검색 검색
+    {
+      console.log(`%${data.term}%`, data, "리포지토리 진입");
+      const results = await this.eventSearchRepository
+        .createQueryBuilder('searchEvents')
+        .where('searchEvents.title LIKE :s OR searchEvents.content LIKE :s', { s: `%${data.term}%` })
         .getMany();
       console.log(results);
       return results
     }
   }
 
-  async findusers(data: any) {
+  async findClubPosts(data: any) : Promise<Clubs[]> { //clubs 게시글 검색
+    {
+      console.log(`%${data.term}%`, data, "리포지토리 진입");
+      const results = await this.clubsSearchRepository
+        .createQueryBuilder('searchClubs')
+        .where('searchClubs.title LIKE :s OR searchClubs.content LIKE :s', { s: `%${data.term}%` })
+        .orderBy("search.id", "DESC")  //최신순(내림차순)
+        .getMany();
+      console.log(results, '리포지토리 통과');
+      return results
+    }
+  }
+
+
+  async findUsers(data: any) : Promise<Users[]>{
     {
       console.log(`%${data.term}%`, data, "리포지토리 진입");
       const results = await this.userSearchRepository
