6784d1f1b29b6b87db3cf70b5985501ca264aed7
ByunJungMin <cvy5375@naver.com>
2023-03-14 02:58:07 +0900

    [수정] jwt middleware 변경

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -3,10 +3,8 @@ import {
   Injectable,
   NotFoundException,
 } from "@nestjs/common";
-import { JwtService } from "@nestjs/jwt";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Users } from "src/entities/users.entity";
-import { RedisService } from "src/redis/redis.service";
 import { Repository } from "typeorm";
 
 @Injectable()
@@ -14,8 +12,6 @@ export class AuthRepository {
   constructor(
     @InjectRepository(Users)
     private readonly userRepository: Repository<Users>,
-    private readonly jwtService: JwtService,
-    private readonly redisService: RedisService,
   ) {}
 
   async createUser(
@@ -32,8 +28,8 @@ export class AuthRepository {
     }
 
     if (nickname) {
-      console.log("hello")
-      throw new BadRequestException("이미 존재하는 닉네임 입니다.",);
+      console.log("hello");
+      throw new BadRequestException("이미 존재하는 닉네임 입니다.");
     }
 
     if (!findemail && !nickname) {
@@ -45,7 +41,6 @@ export class AuthRepository {
         type: "user",
       });
     }
-
   }
 
   async login(email: string) {
@@ -58,17 +53,7 @@ export class AuthRepository {
       throw new NotFoundException("회원이 존재하지 않습니다.");
     }
 
-    const accessToken = await this.jwtService.signAsync({ id: user.userId });
-    const refreshToken = await this.jwtService.signAsync({ email: user.email });
-    console.log("acc   " + accessToken);
-    console.log("ref   " + refreshToken);
-
-    await this.redisService.setRefreshToken(
-      user.userId.toString(),
-      refreshToken,
-    );
-
-    return { ...user, accessToken };
+    return user;
   }
 
   async checkThisUser(userId: number) {
@@ -97,9 +82,9 @@ export class AuthRepository {
 
   async checkemail(email: string) {
     const findemail = await this.userRepository.findOne({
-      where: { email, deletedAt: null}
-    })
+      where: { email, deletedAt: null },
+    });
 
-    return findemail
+    return findemail;
   }
 }
