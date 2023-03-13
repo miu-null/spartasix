aec3aba2f84b661d5ac6f485ae6480368ba99037
vividnow <belucent0@gmail.com>
2023-03-13 17:06:23 +0900

    [수정] 프론트엔드 경로 변경-sign 미들웨어 예외

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { NestExpressApplication } from "@nestjs/platform-express";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { join } from "path";
-const ejslayouts = require("express-ejs-layouts");
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -11,12 +10,11 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   //express-ejs-layouts 설정
-  // app.use(ejslayouts);
-  app.set("layout", "main.ejs");
+  app.set("layout", "../main.ejs");
 
   //ejs 셋팅
   app.useStaticAssets(join(__dirname, "../", "views", "static")); //static폴더를 현재폴더(dirname)에서 한칸 상위('..')에 있는 views/public 폴더로 지정
-  app.setBaseViewsDir(join(__dirname, "../", "views")); //view폴더 지정 :현재폴더(dirname)에서 한칸 상위('..')에 있는 views폴더로 지정
+  app.setBaseViewsDir(join(__dirname, "../", "views", "templates")); //view폴더 지정 :현재폴더(dirname)에서 한칸 상위('..')에 있는 views폴더로 지정
   app.setViewEngine("ejs"); //view엔진을 ejs로 셋팅
 
   const PORT = process.env.PORT;
