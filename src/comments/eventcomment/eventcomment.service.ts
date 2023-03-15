1dde8be23eaaeedbde1a3336dd4f1ec2f25da9c0
ByunJungMin <cvy5375@naver.com>
2023-03-16 06:59:47 +0900

    [추가] 마이페이지 클럽멤버 신청,조회,수정 프론트 연결 완료

--- src/comments/eventcomment/eventcomment.service.ts
+++ src/comments/eventcomment/eventcomment.service.ts
@@ -7,5 +7,25 @@ export class EventCommentService {
     private readonly eventCommentRepository: EventCommentRepository,
   ) {}
 
-  async createComment(data: string) {}
+  async showAllComment() {
+    const comments = await this.eventCommentRepository.showAllComment();
+
+    return comments;
+  }
+
+  async createComment(userId: number, eventPostId: number, content: string) {
+    this.eventCommentRepository.createComment(userId, eventPostId, content);
+  }
+
+  async updateComment(userId: number, eventCommentId: number, content: string) {
+    await this.eventCommentRepository.updateComment(userId, eventCommentId, content);
+
+    return true;
+  }
+
+  async deleteComment(userId: number, eventCommentId: number) {
+    await this.eventCommentRepository.deleteComment(userId, eventCommentId);
+
+    return true;
+  }
 }
