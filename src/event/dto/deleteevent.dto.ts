85fbf7c1ef863085967bf51308b07738062a506f
kangharu <harucometrue7@gmail.com>
2023-03-16 22:24:36 +0900

    이벤트 삭제 기능 진행중

--- src/event/dto/deleteevent.dto.ts
+++ src/event/dto/deleteevent.dto.ts
@@ -3,14 +3,5 @@ import { IsNumber, IsString } from 'class-validator';
 export class DeleteEventDto {
 
   @IsNumber()
-  readonly userId: number;
-
-  @IsString()
-  readonly title: string;
-
-  @IsString()
-  readonly content: string;
-
-  @IsNumber()
-  readonly password: number;
+  readonly eventPostId: number;
 }
