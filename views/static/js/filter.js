3cd69849d378ff03e46a8bb9b7a5f92671d90a4d
vividnow <belucent0@gmail.com>
2023-03-25 18:16:51 +0900

    [수정] 게시물 시간대 gettimeZoneOffset() 적용

--- views/static/js/filter.js
+++ views/static/js/filter.js
@@ -4,9 +4,10 @@ const { format } = require("date-fns-tz");
 // 게시글 정보 조회시 날짜 형식 변경(게시판, 게시글, 댓글, 이전다음 글 안내)
 function reformPostDate(postObjDate) {
   const currentDate = new Date();
+  const offset = currentDate.getTimezoneOffset()* 60000;
+  const postDate = new Date(postObjDate - offset);
 
-  const postDate = new Date(postObjDate.setHours(postObjDate.getHours() +9));
-//   gettimezoneoffset 60000????
+  // const postDate = new Date(postObjDate.setHours(postObjDate.getHours() +9));
   if (isSameDay(postDate, currentDate)) {
     return format(postDate, "kk:mm");
   } else {
