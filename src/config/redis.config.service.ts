a7f4dffb4fbf5ce447b9bea810a71d0ee318235d
vividnow <belucent0@gmail.com>
2023-03-29 14:31:57 +0900

    [수정] 활동왕 포인트 자릿수 보정

--- src/config/redis.config.service.ts
+++ src/config/redis.config.service.ts
@@ -12,9 +12,9 @@ export class CacheConfigService implements CacheOptionsFactory {
   createCacheOptions(): CacheModuleOptions<Record<string, any>> {
     return {
       store: redisStore,
-      username: this.configService.get<string>("REDIS_USERNAME"), // 발표진행시 cloud 연결용
-      password: this.configService.get<string>("REDIS_PASSWORD"), // 발표진행시 cloud 연결용
-      host: "redis-12571.c273.us-east-1-2.ec2.cloud.redislabs.com",
+      // username: this.configService.get<string>("REDIS_USERNAME"), // 발표진행시 cloud 연결용
+      // password: this.configService.get<string>("REDIS_PASSWORD"), // 발표진행시 cloud 연결용
+      localhost: "redis-12571.c273.us-east-1-2.ec2.cloud.redislabs.com",
       port: this.configService.get<number>("REDIS_PORT"),
       ttl: this.configService.get<number>("REDIS_TTL"),
     };
