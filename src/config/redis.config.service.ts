e986a8d1c9199ec18af31319fe06ffa24ae1e96f
lsjnl22 <lsjnl22@gmail.com>
2023-03-30 20:41:32 +0900

    redis.config파일 수정

--- src/config/redis.config.service.ts
+++ src/config/redis.config.service.ts
@@ -8,13 +8,13 @@ import * as redisStore from "cache-manager-ioredis";
 
 @Injectable()
 export class CacheConfigService implements CacheOptionsFactory {
-  constructor(private configService: ConfigService) {}
+  constructor(private configService: ConfigService) { }
   createCacheOptions(): CacheModuleOptions<Record<string, any>> {
     return {
       store: redisStore,
       username: this.configService.get<string>("REDIS_USERNAME"),
       password: this.configService.get<string>("REDIS_PASSWORD"),
-      localhost: "redis-12571.c273.us-east-1-2.ec2.cloud.redislabs.com",
+      host: "redis-12571.c273.us-east-1-2.ec2.cloud.redislabs.com",
       port: this.configService.get<number>("REDIS_PORT"),
       ttl: this.configService.get<number>("REDIS_TTL"),
     };
